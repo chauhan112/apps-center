c505252579e89e4ec8dfba4c317493d5c98592ba
Raja <rajababuchauhan500@gmail.com>
2026-01-15 22:57:25 +0100

    changes

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import "./style.css";
-import { LocalExpController } from "./rlib/timeline/t2025/july/FileExplorer";
+// import { LocalExpController } from "./rlib/timeline/t2025/july/FileExplorer";
+// import { Page } from "./rlib/timeline/t2025/dec/DomainOpsFrontend";
+import { DeploymentCenter } from "./rlib/timeline/t2026/DeploymentCenter";
+// import { Page as DeploymentPage } from "./rlib/timeline/t2025/dec/deployment";
 
-
-
-const fe = LocalExpController();
-
+const page = DeploymentCenter();
+// const page = DeploymentPage();
 document
     .querySelector<HTMLDivElement>("#app")!
-    .appendChild(fe.comp.getElement());
+    .appendChild(page.comp.getElement());
+
+// const page = Page();
+// document
+//     .querySelector<HTMLDivElement>("#app")!
+//     .appendChild(page.getElement());
