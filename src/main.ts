569d1aaac6b563f931a7d8d129deaeb638848aa7
Raja <rajababuchauhan500@gmail.com>
2026-01-17 13:18:39 +0100

    bun build

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,7 @@
 import "./style.css";
-// import { LocalExpController } from "./rlib/timeline/t2025/july/FileExplorer";
-// import { Page } from "./rlib/timeline/t2025/dec/DomainOpsFrontend";
 import { DeploymentCenter } from "./rlib/timeline/t2026/DeploymentCenter";
-// import { Page as DeploymentPage } from "./rlib/timeline/t2025/dec/deployment";
 
 const page = DeploymentCenter();
-// const page = DeploymentPage();
 document
     .querySelector<HTMLDivElement>("#app")!
     .appendChild(page.comp.getElement());
-
-// const page = Page();
-// document
-//     .querySelector<HTMLDivElement>("#app")!
-//     .appendChild(page.getElement());
